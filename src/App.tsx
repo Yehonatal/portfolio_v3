d9e127ef487bc8417fc65978cab309123bdd6b7a
yonatan afewerk <yonatanafewerk@gmail.com>
2024-02-02 17:39:03 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <a href="https://yehonatal.vercel.app/">
+                <h1 className="text-4xl font-extrabold text-blue-600 underline underline-offset-8">
+                    YONATAN AFEWERK TESHOME
+                </h1>
+            </a>
+        </>
+    );
+}
+
+export default App;
