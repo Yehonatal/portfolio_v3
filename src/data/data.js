9863866f8fbe253e00e2817c7b896eac2701a395
yonatan afewerk <yonatanafewerk@gmail.com>
2024-02-03 12:42:59 +0300

    Setup: 😇

---
+++ src/data/data.js
@@ -0,0 +1,30 @@
+const displayName = "Yonatan Afewerk Teshome";
+const bannerDescription =
+    "Well, I'm just a small-time Dev, trying to learn a bunch of stuff from who I can and I make stuff along the way";
+const mediumUrl = "https://medium.com/@yonatanafewerk";
+const email = "yonatanafewerk@gmail.com";
+const githubUrl = "https://github.com/yehonatal";
+const linkedInUrl = "https://www.linkedin.com/in/yonatan-afewerk/";
+const resumePdfTitle = "Resume.pdf";
+
+const projects = [
+    {
+        title: "",
+        description: "",
+        stack: ["tech1", "tech2", "tech2"],
+        flags: { complete: true },
+        imgName: "",
+        siteUrl: "",
+        codeUrl: "",
+    },
+];
+export {
+    projects,
+    mediumUrl,
+    email,
+    githubUrl,
+    linkedInUrl,
+    displayName,
+    bannerDescription,
+    resumePdfTitle,
+};
